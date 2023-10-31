21dd2d22fdd7a556c604beb27700af5eb8ad451b
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-10-31 15:04:47 -0300

    :construction: Python

--- exercícios/002/exercicio.py
+++
@@ -1,6 +0,0 @@
-# Faça um programa que leia um nome de uma pessoa e mostre uma mensagem de bem-vindo(a)
-
-# Declarar uma variável
-nome = input('Digite seu nome:')
-# Saída formatada com a ferramenta ".Format(sua variável)"
-print('É um prazer te conhecer {}!'.format(nome))
--- exercícios/004/exercicio.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que leia algo do teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela
-
-a = input('Digite algo:')
-print('O tipo primitivo desse valor é', type(a))
-print('Só tem espaços?', a.isspace())
-# Utilizar vírgula antes de instanciar um método
-print('É um número?', a.isnumeric())
-print('É alfabético?', a.isalpha())
-print(' É afanúmerico', a.isalnum())
