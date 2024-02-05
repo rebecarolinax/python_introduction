9c1541fb9b15adf8f79dc34fe7b70229c76a3755
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 15:08:22 -0300

    Python

--- exercícios/003/exercicio.py
+++
@@ -1,8 +0,0 @@
-# Crie um programa que leia dois números e traga o resultado deles
-n1 = int(input('Digite um valor:'))
-# Declarando int ou "Tipo primitivo"
-n2 = int(input('Digite outro valor:'))
-# Declarando a soma com a variável "s"
-s = n1 + n2
-# Exibe o valor no terminal
-print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
